5bb255f5512d42304dfee10f476005e9779651b8
trabelsi <ge25zoh@tum.de>
2020-11-17 16:05:34 +0100

    Added bfs, dfs, A* and visualization

--- Graph.py
+++ Graph.py
@@ -1,13 +1,14 @@
 from collections import deque
 
+
 class Graph(object):
-    # A graph is represented as a map: 
+    # A graph is represented as a map:
     # Key: Name of the vertex.
-    # Value: Set of its neighbours and their respective distances.
+    # Value: Set of its neighbours and their respective distance.
 
     #E.g: edges = {"a" : [("b",2), ("c",3)], "b": [("c",5)]}
 
-    def __init__(self, edges = {}):
+    def __init__(self, edges={}):
         """ initialize graph with given dictionay and coordinates.
         """
         self.edges = edges
@@ -27,18 +28,16 @@ class Graph(object):
         for e in self.edges:
             for i in self.edges[e]:
                 s.append(str(e) + " -> " + str(i[0]) + ", cost = " + str(i[1]))
-        
-        return '\n'.join(s)
 
+        return '\n'.join(s)
 
     def __str__(self):
         return "Vetices: " + str(self.vertices) + "\nEdges:\n" + self.get_edges()
 
-    def add_vertex(self,vertex):
+    def add_vertex(self, vertex):
         if vertex in self.vertices:
             return
         self.vertices.add(vertex)
-
 
     def add_edge(self, src, dest, cost=1):
         if src not in self.vertices:
@@ -47,7 +46,7 @@ class Graph(object):
             self.edges[src] = []
         if dest not in self.vertices:
             self.vertices.add(dest)
-        self.edges[src].append((dest,cost))
+        self.edges[src].append((dest, cost))
 
     def get_neighbours(self, vertex):
         if vertex not in self.vertices:
@@ -66,7 +65,6 @@ class Graph(object):
             for i in self.edges[e]:
                 if i[0] == vertex:
                     self.edges[e].remove(i)
-        
 
     def remove_edge(self, src, dest):
         if src not in self.edges:
@@ -75,62 +73,169 @@ class Graph(object):
             if e[0] == dest:
                 self.edges[src].remove(e)
 
+    def dfs_traversal(self, src, visited=[]):
+        print(src)
+        for node in self.get_neighbours(src):
+            if (node[0] not in visited):
+                visited.append(node[0])
+                self.dfs_traversal(node[0], visited)
 
-    def dijkstra(self,src,dest):
+    # performs a dfs search and returns the path
+    def dfs(self, src, des):
+        stack = [(src, [src])]
+        visited = set()
+        visited_ordered = [src]
+        while stack:
+            (vertex, path) = stack.pop()
+            if vertex not in visited:
+                if vertex == des:
+                    return [path, visited_ordered]
+                visited.add(vertex)
+                visited_ordered.append(vertex)
+                for node in self.get_neighbours(vertex):
+                    stack.append((node[0], path + [node[0]]))
+
+        print("No path has been found")
+        return None
+
+    # performs bfs search and returns the path
+    def bfs(self, src, des):
+        queue = [(src, [src])]
+        visited = set()
+        visited_ordered = [src]
+
+        while queue:
+            (vertex, path) = queue.pop(0)
+            if vertex not in visited:
+                if vertex == des:
+                    return [path,visited_ordered]
+                visited.add(vertex)
+                visited_ordered.append(vertex)
+                for (node, cost) in self.get_neighbours(vertex):
+                    queue.append((node, path + [node]))
+
+        print("No path has been found")
+        return None
+
+    def dijkstra(self, src, dest):
         if src not in self.vertices or dest not in self.vertices:
             return None
 
         inf = float('inf')
-        
-        #distance to source = 0; distance to rest nodes = infinity
-        dist = {vertex : inf for vertex in self.vertices}
+
+        # distance to source = 0; distance to rest nodes = infinity
+        dist = {vertex: inf for vertex in self.vertices}
         dist[src] = 0
-        #initialize previous:
-        prev = {vertex : None for vertex in self.vertices}
-        #neighbours of each vertex:
-        neighbours = {vertex : self.get_neighbours(vertex) for vertex in self.vertices}
-        
-        q = self.vertices.copy()
+        # initialize previous:
+        prev = {vertex: None for vertex in self.vertices}
+        # neighbours of each vertex:
+        neighbours = {vertex: self.get_neighbours(
+            vertex) for vertex in self.vertices}
+
+        # set of visited nodes
+        visited = set()
+        visited.add(src)
+        visited_ordered = [src]
+        q = [src]
 
         while q:
-            next = min(q,key = lambda v: dist[v])
-            #remove element with shortest distance
+            next = min(q, key=lambda v: dist[v])
+            # remove element with shortest distance
             q.remove(next)
-            #if we reached destination or smallest distance is infinite then break
+            # if we reached destination or smallest distance is infinite then break
             if next == dest:
                 break
 
-            if dist[next] == inf:
+            for v, cost in neighbours[next]:
+                if v not in visited:
+                    visited.add(v)
+                    visited_ordered.append(v)
+                    q.append(v)
+                    new_dist = dist[next] + cost
+                    if new_dist < dist[v]:
+                        dist[v] = new_dist
+                        prev[v] = next
+
+            if len(q) == 0:
                 print("Cannot reach destination!")
                 return ""
 
-            for v,cost in neighbours[next]:
-                new_dist = dist[next] + cost
-                if new_dist < dist[v]:
-                    dist[v] = new_dist
-                    prev[v] = next
-
-        
-        #build the path
+        # build the path
         path = deque()
 
         while prev[dest]:
             path.appendleft(dest)
             dest = prev[dest]
 
-        #add source to beginning of the path
+        # add source to beginning of the path
         path.appendleft(src)
 
         #print("The shortest path is: " + " -> ".join(path) + "\ncost = "+ str(dist[next]))
-        return path
+        return [path, visited_ordered]
+
+    def a_star(self, src, dest, distances):
+
+        if src not in self.vertices or dest not in self.vertices:
+            return None
+
+        inf = float('inf')
+
+        # distance to source = 0; distance to rest nodes = infinity
+        dist = {vertex: inf for vertex in self.vertices}
+        dist[src] = 0
+        # initialize previous:
+        prev = {vertex: None for vertex in self.vertices}
+        # neighbours of each vertex:
+        neighbours = {vertex: self.get_neighbours(
+            vertex) for vertex in self.vertices}
+
+        # set of visited nodes
+        visited = set()
+        visited.add(src)
+        visited_ordered = [src]
+        q = [src]
+
+        while q:
+            next = min(q, key=lambda v: dist[v] + distances[v])
+            # remove element with shortest distance
+            q.remove(next)
+            # if we reached destination or smallest distance is infinite then break
+            if next == dest:
+                break
+
+            for v, cost in neighbours[next]:
+                if v not in visited:
+                    visited.add(v)
+                    visited_ordered.append(v)
+                    q.append(v)
+                    new_dist = dist[next] + cost
+                    if new_dist < dist[v]:
+                        dist[v] = new_dist
+                        prev[v] = next
+
+            if len(q) == 0:
+                print("Cannot reach destination!")
+                return ""
+
+        # build the path
+        path = deque()
+
+        while prev[dest]:
+            path.appendleft(dest)
+            dest = prev[dest]
+
+        # add source to beginning of the path
+        path.appendleft(src)
+
+        #print("The shortest path is: " + " -> ".join(path) + "\ncost = "+ str(dist[next]))
+        return [path, visited_ordered]
 
 
 #####################################################################################################################
 
+g = Graph({"a": [("b", 7), ("c", 9), ("f", 14)], "b":  [("c", 10),
+                                                        ("d", 15)], "c": [("d", 11), ("f", 2), ("x", 9)],  "d": [("e", 6)],
+           "e": [("f", 9)]})
 
-#g = Graph({"a" : [("b", 7),("c", 9),("f", 14)], "b":  [("c", 10),
-#            ("d", 15)], "c": [("d", 11), ("f", 2)],  "d": [("e", 6)],
-#             "e" :[("f", 9)]})
-
-#print(g)
-#print(g.dijkstra("a","e"))
+# print(g.get_neighbours("a"))
+#print(g.dfs("a", "e"))
--- Main.py
+++ Main.py
@@ -1,66 +1,83 @@
 from collections import deque
 import pygame
 import Graph as graph
+import math
 
 pygame.init()
 
+
 class grid(object):
-    def __init__ (self, w, h, rows):
+    def __init__(self, w, h, rows):
         self.w = w
         self.h = h
         self.rows = rows
         self.s = self.h // rows
         self.cols = self.w // self.s
+        self.graph = self.generate_graph()
 
-    def draw_grid (self,surface):
-        w = self.h // self.rows 
+    def draw_grid(self, surface):
+        w = self.h // self.rows
 
-        #horizontal:
-        for i in range (self.rows + 1):
-           pygame.draw.line(surface,(105,105,105),(0,i*w), (self.w,i*w),2)
-        
-        #vertical:
+        # horizontal:
+        for i in range(self.rows + 1):
+            pygame.draw.line(surface, (105, 105, 105),
+                             (0, i*w), (self.w, i*w), 1)
+
+        # vertical:
         i = 0
         while (i*w < self.w):
-            pygame.draw.line(surface,(105,105,105),(i*w,0), (i*w,self.h),2)
-            i+=1
-
+            pygame.draw.line(surface, (105, 105, 105),
+                             (i*w, 0), (i*w, self.h), 2)
+            i += 1
 
     def generate_graph(self):
-        #geneates a graph object representing the grid.
+        # geneates a graph object representing the grid.
         g = graph.Graph()
         num_cells = self.rows * self.cols
-        for i in range (num_cells):
+        for i in range(num_cells):
             g.add_vertex(str(i))
 
-        for i in range (num_cells):
+        for i in range(num_cells):
             if i // self.cols == (i+1) // self.cols:
-                g.add_edge(str(i),str(i+1))
+                g.add_edge(str(i), str(i+1))
             if i-1 >= 0 and i // self.cols == (i-1) // self.w:
-                g.add_edge(str(i),str(i-1))
+                g.add_edge(str(i), str(i-1))
             if i + self.cols < num_cells:
-                g.add_edge(str(i),str(i + self.cols))
+                g.add_edge(str(i), str(i + self.cols))
             if i - self.cols >= 0:
-                g.add_edge(str(i),str(i - self.cols))
+                g.add_edge(str(i), str(i - self.cols))
 
         return g
 
-    def draw_vertex(self,window,vertex,color):
-        (r,c) = (int(vertex) // self.cols, int(vertex) % self.cols)
-        pygame.draw.rect(window, color, (c*self.s,r*self.s,self.s,self.s))
+    def draw_vertex(self, window, vertex, color):
+        (r, c) = (int(vertex) // self.cols, int(vertex) % self.cols)
+        pygame.draw.rect(window, color, (c*self.s, r *
+                                         self.s, self.s-0.5, self.s-0.5))
+
+    def manhattanDistance(self, v1, v2):
+        (r1, c1) = (int(v1) // self.cols, int(v1) % self.cols)
+        (r2, c2) = (int(v2) // self.cols, int(v2) % self.cols)
+        return abs(r1-r2) + abs(c1-c2)
+
+    def computeDistances(self, goal):
+        vertices = self.graph.get_vertices()
+        map = dict()
+        for v in vertices:
+            map[v] = self.manhattanDistance(v, goal)
+        return map
 
 
 running = True
-#window dimensions:
+# window dimensions:
 w = 1200
 h = 900
 num_rows = 30
 
-window = pygame.display.set_mode((w,h))
-window.fill((255,255,255))
+window = pygame.display.set_mode((w, h))
+window.fill((255, 255, 255))
 pygame.display.set_caption("Path Finder")
-grid = grid(w,h,num_rows)
-grph = grid.generate_graph()
+grid = grid(w, h, num_rows)
+grph = grid.graph
 
 
 finished = False
@@ -83,46 +100,46 @@ while running:
     if not config:
         if start == -1 or end == -1:
             if mouse[0]:
-                (x,y) = pygame.mouse.get_pos()
+                (x, y) = pygame.mouse.get_pos()
                 if x >= 0 and x < grid.w and y >= 0 and y < grid.h:
                     vertex = str((y // grid.s) * grid.cols + (x // grid.s))
-                    grid.draw_vertex(window,vertex,(255,0,0))
+                    grid.draw_vertex(window, vertex, (255, 0, 0))
+
                     pygame.display.update()
-                
+
                 if start == -1:
                     start = vertex
                 elif vertex != start:
                     end = vertex
+
         else:
             if mouse[0]:
-                (x,y) = pygame.mouse.get_pos()
+                (x, y) = pygame.mouse.get_pos()
                 if x >= 0 and x < grid.w and y >= 0 and y < grid.h:
                     vertex = str((y // grid.s) * grid.cols + (x // grid.s))
-                    if vertex != start and vertex != end:    
-                        grid.draw_vertex(window,vertex,(0,0,0))
+                    if vertex != start and vertex != end:
+                        grid.draw_vertex(window, vertex, (100, 100, 100))
                         grph.remove_vertex(vertex)
             if keys[pygame.K_RETURN]:
                 config = True
 
-
     if config and not finished:
-        path = grph.dijkstra(start,end)
-        
+        distances = grid.computeDistances(end)
+        #path = grph.a_star(start,end, distances)
+        path, order = grph.a_star(start, end, distances)
+
         if path:
+            for vertex in order:
+                if vertex != start and vertex != end:
+                    grid.draw_vertex(window, vertex, (247, 247, 89))
+                pygame.time.delay(50)
+                pygame.display.update()
             for vertex in path:
                 if vertex != start and vertex != end:
-                    grid.draw_vertex(window,vertex,(0,0,255))
+                    grid.draw_vertex(window, vertex, (115, 194, 4))
                 pygame.time.delay(50)
                 pygame.display.update()
             print("The cost of the shortest path is " + str(len(path)-1))
         else:
             print("No path has been found!")
         finished = True
-
-    
-    
-
-    
-    
-
-
